45ab33439c6e10bd02088cbee7f3d0db85f124b9
Wesley atanasio <66285513+wesleyatanasio@users.noreply.github.com>
2021-10-13 22:19:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidestoshow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+
+});
